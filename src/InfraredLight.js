c4fd28c433a0e68d276ef85c96203b686c845c6d
Thomas Ederer <thomas@orchideenvermehrung.at>
2024-04-18 14:02:56 +0100

    replaced python script by independent service

--- src/InfraredLight.js
+++ src/InfraredLight.js
@@ -1,62 +1,46 @@
 /* global octowatch, common, assertNamespace, setTimeout */
 
-const childprocess = require('node:child_process');
+const net = require('node:net'); 
 
 require('./common/logging/LoggingSystem.js');
+require('./LineBasedTcpConnection.js');
+require('./InfraredLight.js');
 
 assertNamespace('octowatch');
 
 octowatch.InfraredLight = function InfraredLight(initialLightLevel) {
-   var LOGGER         = common.logging.LoggingSystem.createLogger('InfraredLight');
-   var thisInstance   = this;
-   var lightLevel     = initialLightLevel;
-   var pythonProcess;
-   var startPythonProcess; // declaring it here to satisfy linter
+   const IR_LIGHT_CONTROL_HOST = '127.0.0.1';
+   const IR_LIGHT_CONTROL_PORT = 8886;
+   
+   var logger       = common.logging.LoggingSystem.createLogger('InfraredLight');
+   var connected    = false;
+   var lightLevel   = initialLightLevel;
+   var thisInstance = this;
+   var connection;
    
    this.setLevel = function setLevel(levelInPercent) {
       lightLevel = levelInPercent;
-      if (pythonProcess !== undefined) {
-         pythonProcess.stdin.write(levelInPercent + '\n');
-      } else {
-         LOGGER.logDebug('ignoring request to set level to ' + levelInPercent + ' because python process is not alive');
+      if (connected) {
+         logger.logInfo('setting level ' + levelInPercent);
+         connection.sendCommand(levelInPercent);
       }
    };
    
-   var restartPythonProcess = function restartPythonProcess() {
-      setTimeout(() => {
-         LOGGER.logInfo('trying to restart python process in 1000 ms');
-         startPythonProcess();
-      }, 1000);
-   };   
-   
-   startPythonProcess = function startPythonProcess() {
-      pythonProcess = childprocess.exec('sudo ./setIrLightLevel', (error, stdout, stderr) => {
-         if (error) {
-            LOGGER.logError('failed to start python code: ' + error);
-            LOGGER.logError('stderr: ' + stderr);
-         }
-      });
-      
-      pythonProcess.on('spawn', () => {
-         LOGGER.logInfo('python process spawned successfully');
-         thisInstance.setLevel(lightLevel);  
-      });
-      
-      pythonProcess.on('close', code => {
-         LOGGER.logInfo('python process closed with exit code ' + code);
-         restartPythonProcess();      
-      });
-      
-      pythonProcess.on('exit', code => {
-         LOGGER.logInfo('python process exited with exit code ' + code);
-         restartPythonProcess();      
-      });
-      
-      pythonProcess.on('error', error => {
-         LOGGER.logError('error in python process: ' + error);
-         restartPythonProcess();      
-      });
+   this.onConnected = function onConnected() {
+      if (!connected) {
+         connected = true;
+         logger.logInfo('connected');
+         thisInstance.setLevel(lightLevel);
+      }
    };
    
-   startPythonProcess();
+   this.onDisconnected = function onDisconnected() {
+      if (connected) {
+         connected = false;
+         logger.logInfo('disconnected');
+      }
+   };
+   
+   connection = new octowatch.LineBasedTcpConnection(IR_LIGHT_CONTROL_HOST, IR_LIGHT_CONTROL_PORT);
+   connection.start(this);
 };
